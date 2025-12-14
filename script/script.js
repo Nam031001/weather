7dbde6e031a9c0880a63e964b5c501c907eeb43f
Nam031001 <127704144+Nam031001@users.noreply.github.com>
2025-12-15 01:02:41 +0900

    ////

--- script/script.js
+++ script/script.js
@@ -5,6 +5,10 @@ button.addEventListener('click', () => {
 });
 //button 정보 받아오기
 
+const tempSection = document.querySelector('.temperature');
+const placeSection = document.querySelector('.place');
+const descSection = document.querySelector('.description');
+
 button.addEventListener('click', () => {
   navigator.geolocation.getCurrentPosition(success, fail);
 });
@@ -20,14 +24,24 @@ const success = (position) => {
   getWeather(latitude, longitude);
 };
 
-
 const API_KEY = 'd399bb539b9cfd6d01c40c127f04881c';
 
 const getWeather = (lat, lon) => {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`
   );
+  then((response) => {
+    return response.json();
+  });
+  then((json) => {
+    const temperature = json.main.temp;
+    const place = json.name;
+    const description = json.weather[0].description;
+
+    tempSection.innerText = temperature;
+    placeSection.innerText = place;
+    descSection.innerText = description;
+  });
+  //json으로 변환 (fetch를 사용시 응답데이터를 JSON으로 인코딩해야 사용가능)
 };
 //위치 좌표 값 받아오기
-
-
